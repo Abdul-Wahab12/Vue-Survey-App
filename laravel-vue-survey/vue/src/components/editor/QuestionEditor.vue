f66b7ac90a0355a1b0ee798e2c6ea4dd5b3e89e1
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2023-11-29 02:27:17 +0500

    Added: add options list section in QuestionEditor component

--- laravel-vue-survey/vue/src/components/editor/QuestionEditor.vue
+++ laravel-vue-survey/vue/src/components/editor/QuestionEditor.vue
@@ -97,6 +97,61 @@
     <!--/ Question Description -->
 
     <!-- Data -->
+    <div>
+        <div v-if="shouldHaveOptions()" class="mt-2">
+            <h4 class="text-sm font-semibold mb-1 flex justify-between items-center">
+                Options
+                <!-- Add new option -->
+                <button
+                    type="button"
+                    @click="addOption()"
+                    class="flex items-center text-xs py-1 px-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    Add Option
+                </button>
+                <!--/ Add new option -->
+            </h4>
+
+            <div 
+                v-if="!model.data.options.length"
+                class="text-xs text-gray-600 text-center py-3"
+            >
+                You don't have any question defined
+            </div>
+            <!-- Option list -->
+            <div
+                v-for="(option, index) in model.data.options"
+                :key="option.uuid"
+                class="flex items-center mb-1"
+            >
+                <span class="w-6 text-sm">{{ index + 1 }}.</span>
+                <input 
+                    type="text"
+                    v-model="option.text"
+                    @change="dataChange"
+                    class="w-full rounded-sm py-1 px-2 text-xs border border-gray-300 focus:border-indigo-500"
+                />
+                <!-- Delete option -->
+                <button
+                    type="button"
+                    @click="removeOption(option)"
+                    class="h-6 w-6 rounded-full flex items-center justify-center border border-transparent transition-colors hover:border-red-100"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                </button>
+                <!--/ Delete option -->
+            </div>
+            <!--/ Option list -->
+        </div>
+    </div>
     <!--/ Data -->
 </template>
 
